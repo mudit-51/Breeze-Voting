9155f0e3aefdb885e55dc7c66c60d9be0f8aed1c
NimanshEndlay <83265413+NimanshE@users.noreply.github.com>
2024-10-22 15:29:04 +0530

    Update page.tsx
    removed statistics

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -198,23 +198,7 @@ export default function Home() {
       {votes && (
         <div className={`${acidg.className} mt-8`}>
           <div className="text-center">Current voting statistics</div>
-          <div className={`flex flex-col mt-4  gap-6 px-10`}>
-            
-            <div className="flex justify-between">
-              <div className="">1. 90's Sensation</div>
-              <div className="">{votes[0]+135} Votes</div>
-            </div>
-            
-            <div className="flex justify-between">
-              <div className="">2. Modern Desi</div>
-              <div className="">{votes[2]+ 17} Votes</div>
-            </div>
-            
-            <div className="flex justify-between">
-              <div className="">3. Techno-Cyberpunk</div>
-              <div className="">{votes[1]+40} Votes</div>
-            </div>
-          </div>
+          <div className="text-center">RESULTS PENDING...</div>
         </div>
       )}
     </div>
